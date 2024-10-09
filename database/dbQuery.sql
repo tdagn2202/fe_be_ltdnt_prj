68dd9fd0e735fd8a877b29c16cc043eacc4c10c7
tdagn2202 <tranhaidang2222004@gmail.com>
2024-10-09 15:31:51 +0700

    Fixing SQLFile

--- database/dbQuery.sql
+++ database/dbQuery.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE Student_Management
+GO
 USE Student_Management
 
 CREATE TABLE Users (
@@ -8,8 +9,8 @@ CREATE TABLE Users (
 );
 
 CREATE TABLE Students (
-    StudentID INT PRIMARY KEY IDENTITY(1,1),
-    UserID INT NOT NULL FOREIGN KEY REFERENCES Users(UserID),
+    UserID INT NOT NULL IDENTITY(1,1) FOREIGN KEY REFERENCES Users(UserID),
+    StudentID INT PRIMARY KEY,
     Name NVARCHAR(100) NOT NULL,
     Age INT NOT NULL,
     Email NVARCHAR(100) NOT NULL UNIQUE,
@@ -40,3 +41,35 @@ CREATE TABLE Courses (
     CourseName NVARCHAR(100) NOT NULL,
     Credits INT
 );
+
+INSERT INTO Users (Username, Password) values ('sa', 'sa') 
+INSERT INTO Students 
+(
+    StudentID, Name, Age, Email, Address, PhoneNumber
+)
+VALUES 
+(
+    'B2203551', 'Tran Hai Dang', 20, 'dangB2203551@student.ctu.edu.vn', 'Hau Giang', 0123456789
+) 
+
+INSERT INTO Schedules 
+(
+    StudentID, Course, DayOfWeek, StartTime, EndTime
+)
+VALUES
+(
+    'B2203551', 'React Native', 'Wednesday', '13:30', '16:10'
+) 
+
+INSERT INTO Grades 
+(
+    StudentID, Course, Grade, DateAssigned
+)
+VALUES 
+(
+    'B2203551', 'React Native', 'A', GETDATE()
+)
+
+INSERT INTO Courses (CourseName, Credits) values ('React Native', 3) 
+
+SELECT * FROM Students
